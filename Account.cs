43671b9ad5bbed17a91242372da557f07d7f9c37
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-27 11:55:47 -0600

    Update Account.cs

--- Account.cs
+++ Account.cs
@@ -2,5 +2,14 @@
 
 public class Account {
    // Add your code here
+   public static string Name;
+   public static string ChangeName(string NewName){
+      Name = NewName;
+      return NewName;
+   }
+   static NameChange(){
+      Name = "Main Account";
+   }
 }
+
 
