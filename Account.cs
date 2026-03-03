54716902ad5db52d77a64a498354bc2989517323
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-03 17:02:59 -0600

    Refactor ChangeName method parameter to camelCase

--- Account.cs
+++ Account.cs
@@ -3,31 +3,15 @@
 public class Account {
    // Add your code here
    public static string Name;
+   
    static NameChange(){
       Name = "Main Account";
    }
-   public static string ChangeName(string NewName){
-      Name = NewName;
+   
+   public static string ChangeName(string newName){
+      Name = newName;
       return Name;
    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
