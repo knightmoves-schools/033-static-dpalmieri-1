f989f43a79a5824089b6900aa9c802eb1fba2e17
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-03 16:55:53 -0600

    Refactor Account class structure and initialization

--- Account.cs
+++ Account.cs
@@ -2,15 +2,16 @@
 
 public class Account {
    // Add your code here
-   public static string Name = "";
+   public static string Name;
+   static NameChange(){
+      Name = "Main Account";
+   }
    public static string ChangeName(string NewName){
       Name = NewName;
       return NewName;
    }
-   static NameChange(){
-      Name = "Main Account";
-   }
 }
+
 
 
 
